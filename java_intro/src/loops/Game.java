c27849df097695661c3fbdc2913a25f93cacda0f
vattikoti-sai <saivattikoti2@gmail.com>
2026-09-15 09:54:08 +0530

    committed on 15-09-26

---
+++ java_intro/src/loops/Game.java
@@ -0,0 +1,31 @@
+package loops;
+import java.util.Random;
+import java.util.Scanner;
+public class Game {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random d = new Random();
+		int s=d.nextInt(1,10);
+
+int chances=2;
+int i=3;
+while(i>0) {
+	System.out.println("enter your number : ");
+	int n=sc.nextInt();
+	if(s==n) {
+		System.out.println(" you won ");
+		break;
+	}else {
+		
+		System.out.println("better luck next time "+chances+"more chances left");
+	}
+	i--;
+	chances--;
+}
+
+
+		
+	}
+
+}
